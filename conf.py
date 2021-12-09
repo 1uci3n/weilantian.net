45a9e0aa6df90843a1cf3753f93ebea83a7d4a13
1uci3n <giranten@outlook.com>
2021-12-09 17:18:59 +0900

    init CDN

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "1uci3n.github.io/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
