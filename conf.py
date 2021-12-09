dc7d4099d6713732780dda1c4b18282d2207dcfd
1uci3n <giranten@outlook.com>
2021-12-09 17:23:50 +0900

    fix CDN uri

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ template = {
 }
 enable_jsdelivr = {
     "enabled": True,
-    "repo": "1uci3n.github.io/Blog-With-GitHub-Boilerplate@gh-pages"
+    "repo": "1uci3n/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
